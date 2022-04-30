5d7fd40e8a8c2f1e3846f0a3e7aa0a46e70f1fe7
Jiayu Yi <yijiayu@gmail.com>
2022-04-30 14:03:26 +0800

    Fix Cloud Tasks backend not encoding request body

--- django_generic_tasks/backends.py
+++ django_generic_tasks/backends.py
@@ -19,13 +19,12 @@ class ThreadingBackend:
 
 
 class CloudTasksBackend:
-    def __init__(self, project, location, queue, base_url, service_account, audience):
+    def __init__(self, project, location, queue, base_url, service_account):
         self.project = project
         self.location = location
         self.queue = queue
         self.base_url = base_url
         self.service_account = service_account
-        self.audience = audience
 
     def run(self, task: Task):
         client = tasks_v2.CloudTasksClient()
@@ -37,12 +36,15 @@ class CloudTasksBackend:
         task = {
             "http_request": {
                 "http_method": tasks_v2.HttpMethod.POST,
+                "headers": {
+                    "content-type": "application/json",
+                },
                 "url": self.base_url + reverse(f"tasks:{task.fully_qualified_name}"),
                 "oidc_token": {
                     "service_account_email": self.service_account,
-                    "audience": self.audience,
+                    "audience": self.base_url,
                 },
-                "body": task.params.json(),
+                "body": task.params.json().encode(),
             }
         }
         client.create_task(request={"parent": parent, "task": task})
